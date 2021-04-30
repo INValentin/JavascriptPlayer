e697503bd239c4cd7c46f2168609de9a9c9d1a67
ISHIMWE Valentin <ishimwevalentin3@gmail.com>
2021-04-30 12:12:12 +0200

    First commit

---
+++ precache-manifest.c9013e5050f102a3457818b1ebe90aed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af81c6fa87607637f2880492966e71a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68c846003a0e55943870",
+    "url": "/static/css/main.bdc1333e.chunk.css"
+  },
+  {
+    "revision": "5c4270f00a5806e1fd9f",
+    "url": "/static/js/2.e4d259b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4d259b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c846003a0e55943870",
+    "url": "/static/js/main.d07db0a2.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
